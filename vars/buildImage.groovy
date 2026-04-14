c2b3004e017e7eb2a79f743665ad41d3e824371c
Haroon Saeed <haroon.saeed@outlook.com>
2026-04-14 18:31:14 +0200

    fix: docker hub info

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,9 +3,9 @@
 def call(){
    echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
+        sh 'docker build -t saeedha/java-maven-app:jma-2.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
+        sh 'docker push saeedha/java-maven-app:jma-2.0'
     }
 
 }
